6d2fa14a441c5cb9b1fcd98c0c6a83ccf18d7f63
Benjamin <navarrobejamin@gmail.com>
2020-10-01 15:36:52 -0300

    crud

---
+++ src/main/java/com/example/demo/PersonaService.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import java.util.List;
+
+
+
+public interface PersonaService {
+    List<Persona>listar();
+    Persona listarId(int id);
+    Persona add(Persona p);
+    Persona edit(Persona p);
+    Persona delete(int id);
+}
---
+++ src/main/java/com/example/demo/WebServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceSpringApplication.class, args);
+	}
+
+}
